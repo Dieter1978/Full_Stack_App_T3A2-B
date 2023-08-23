abbfd0f196124320dce7f5077b9366a47aa0cc21
ahan-nz <14235@coderacademy.edu.au>
2023-08-23 17:30:32 +1000

    added delete student route

--- routes/student_routes.js
+++ routes/student_routes.js
@@ -19,16 +19,15 @@ router.get('/', async (req, res) => {
 // Get one student
 router.get('/:id', async (req, res) => {
     try {
-        const studentId = req.params.id;
-        const student = await StudentModel.findById(studentId)
+        const student = await StudentModel.findById(req.params.id)
           .populate({ path: 'year', select: ' -_id year' })
           .populate({ path: 'class', select: '-_id name' })
     
-        res.send(student)
-
-        if (!student) {
+          if (!student) {
             return res.status(404).send('Student not found.')
-        }
+          }
+        
+          res.send(student)
 
       } catch (error) {
         res.status(500).send({ error: error.message })
@@ -37,12 +36,34 @@ router.get('/:id', async (req, res) => {
 
 // Create a student POST
 // admin only
+// router.post('/', async (req, res) => {
+//   try {
 
+//     // const { firstname, lastname, email, year, class, photo } = req.body
+//     // const newStudent = await StudentModel.create({ firstname, lastname, email, year: year, class, photo })
+//     res.status(201)
+//   } catch (error) {
+//     res.status(500).send({ error: error.message })
+//   }
+// })
 
 // Update a student UPDATE
 // admin and student
 
 // Delete a student DELETE
 // admin only
+router.delete('/:id', async (req,res) => {
+  try {
+      const student = await StudentModel.findByIdAndDelete(req.params.id)
+    
+      if (student){
+          res.sendStatus(200)
+      } else {
+          res.status(404).send({error: 'Student not Found'})
+      }
+  }
+  catch (err)
+    {res.status(500).send(err.message)}
+})
 
 export default router
