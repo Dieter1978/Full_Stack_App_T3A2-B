45ab5aa0acba8a69539ed67c7ea481c31a1155ae
ahan-nz <14235@coderacademy.edu.au>
2023-09-01 13:53:22 +1000

    login route tests

--- routes/auth_routes.test.js
+++ routes/auth_routes.test.js
@@ -1,12 +1,51 @@
 import app from '../app'
 import request from 'supertest'
-// import token from './test_token.js'
 
-// Testing register route
-describe ('Auth Test', () => {
+// Testing login route
+describe ('POST /login', () => {
+	let res
 
-	test ('POST /signup', async () => {
+	beforeEach(async () => {
+		res = await request(app)
+      .post('/login')
+      .send({
+        email: 'bill.smith@gmail.com',
+        password: 'anothertesting',
+      })
+	})
 
+	test ('Existing user can login successfully and receive a token', async () => {
+		expect(res.status).toBe(200)
+    expect(res.body.token).toBeDefined()
+    expect(res.body.message).toBe('Login successful!')
+	})
+
+	test ('Returns the correct user information', async () => {
+		expect(res.body.user.name).toMatch('Bill')
+		expect(res.body.user.email).toMatch('bill.smith@gmail.com')
+		expect(res.body.user.student).toMatch('64f075943d65542d36badbaa')
 	})
 	
 })
+
+// Testing register route
+// describe ('POST /signup', () => {
+// 	let res
+
+// 	beforeEach(async () => {
+// 		res = await request(app)
+//       .post('/signup')
+//       .send({
+// 				name: 'Rex Stevens',
+// 				password: '1234567',
+// 				email: 'rex.stevens@gmail.com',
+// 				studentId: '64f075943d65542d36badbab'
+//       })
+// 	})
+
+// 	test ('User is able to register with valid student ID and receive a token', async () => {
+// 		expect(res.status).toBe(201)
+//     expect(res.body.token).toBeDefined()
+//     expect(res.body.message).toContain('Registration successful')
+// 	})
+// })
